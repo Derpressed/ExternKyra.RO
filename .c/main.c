de0979290891577da8d616ee93820cda95f47467
Derpressed <61562288+Derpressed@users.noreply.github.com>
2026-04-02 00:59:18 -0500

    Update main.c

--- .c/main.c
+++ .c/main.c
@@ -1,17 +1,27 @@
-#include "ProcessParameters.h"
+#include "headers.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
+#define APP_NAME "My_Code.exe"
 
-int main() {
-    Process program = {
-        .appName = "RobloxPlayerBeta.exe"
-    };
+int main(int argc, char *argv[]) {
 
-    getProcessHandle(&program);
-    getProcessModule(&program);
-    printStruct(&program);
-    
-    
+    if (argc >= 2) {
+        Process prog = {
+            .appName = APP_NAME,
+            .progID = atoi(argv[1])
+        };
+
+        getProcessHandle(&prog);
+        getProcessModule(&prog);
+        printStruct(&prog);
+
+        uint8_t buf;
+        int valid = readAtAddr(&prog, (LPCVOID) 0x000000000000, &buf);
+        printf("\n%02X\n", buf);
+    }
+
     getchar();
     return 0;
 }
